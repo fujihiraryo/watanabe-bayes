acb382520105fb082b7ae4a1ae53f615462b71a2
fujihiraryo <fujihiraryo@team-lab.com>
2020-01-08 01:46:04 +0900

    MALA

--- python/metropolis_adjusted_langevin_monte_carlo.py
+++ python/metropolis_adjusted_langevin_monte_carlo.py
@@ -9,33 +9,49 @@ def f(x):
 Z=0.906402*2
 # 目的の分布はexp(-H(x))/Z
 
+def threshold(x,y):
+    P=1
+    P*=np.exp(-H(y))
+    P*=np.exp(-(y-x-eps*f(x))**2/(4*eps))
+    P/=np.exp(-H(x))
+    P/=np.exp(-(x-y-eps*f(y))**2/(4*eps))
+    return P
+
 T=10000
 x=0
 eps=0.1
 X=[]
+accept=0
 for t in range(T):
     g=np.random.normal()
     try:
-        x=x+eps*f(x)+g*(2*eps)**0.5
-        X.append(x)
+        y=x+eps*f(x)+g*(2*eps)**0.5
+        u=np.random.random()
+        if u<threshold(x,y):
+            accept+=1
+        else:
+            y=x
+        X.append(y)
+        x=y
     except:
         break
 T=len(X)
 plt.xlim(-3,3)
 plt.ylim(0,0.7)
 title="t="+str(T)
+title+=", accept_rate="+str(round(accept/T,3))
 plt.title(title)
 plt.hist(X,density=True,bins=100)
 x_range=np.linspace(-3,3,1000)
 y_range=np.exp(-H(x_range))/Z
 plt.plot(x_range,y_range)
 now=dt.now().strftime("%Y%m%d%H%M%S")
-name="image/langevin_monte_carlo"+now+".png"
+name="image/metropolis_adjusted_langevin_monte_carlo"+now+".png"
 plt.savefig(name)
 
 plt.figure()
 plt.ylim(-10,10)
 plt.plot(range(T),X)
 plt.title("eps="+str(eps))
-name="image/langevin_monte_carlo_path"+now+".png"
+name="image/metropolis_adjusted_langevin_monte_carlo_path"+now+".png"
 plt.savefig(name)
